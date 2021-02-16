8a4d6529c7e13d30946596c0c2d7dee989ca2fdf
opencovid-mr <63929979+opencovid-mr@users.noreply.github.com>
2021-02-16 23:57:20 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,13 @@
+import requests
+
+from datetime import datetime
+
+url='https://hub.dati.lombardia.it/resource/7jw9-ygfv.csv'
+
+filename=datetime.today().strftime('%Y-%m-%d')+'.csv'
+
+req = requests.get(url)
+csv_file = open(filename, 'wb')
+
+csv_file.write(req.content)
+csv_file.close()
