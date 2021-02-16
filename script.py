3465e5ece463cf09c9c01a79c74b26b2ba968250
opencovid-mr <63929979+opencovid-mr@users.noreply.github.com>
2021-02-17 00:01:25 +0100

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 url='https://hub.dati.lombardia.it/resource/7jw9-ygfv.csv'
 
-filename=datetime.today().strftime('%Y-%m-%d')+'.csv'
+filename='data/'+datetime.today().strftime('%Y-%m-%d')+'.csv'
 
 req = requests.get(url)
 csv_file = open(filename, 'wb')
